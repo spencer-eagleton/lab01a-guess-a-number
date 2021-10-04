de9e78959912e899de4a982713adff1bd4cf5faf
spencer-eagleton <86867967+spencer-eagleton@users.noreply.github.com>
2021-10-04 15:33:34 -0700

    set click event to grab user input, compare to random number, log result to console.

--- app.js
+++ app.js
@@ -9,6 +9,21 @@ console.log(randomNumber)
 
 
 // set event listeners 
+button.addEventListener('click', ()=> {
+  console.log('click!')
+  console.log(userGuess.value)
   // get user input
+  if(Number(userGuess.value) == randomNumber) {
+      console.log('You guessed it!')
+  } else if (Number(userGuess.value) < randomNumber) {
+      console.log('Guess higher!')
+  } else if (Number(userGuess.value) > randomNumber) {
+      console.log('Too high! Guess again.')
+  }
+  
+  
+  
   // use user input to update state 
   // update DOM to reflect the new state
+});
+
