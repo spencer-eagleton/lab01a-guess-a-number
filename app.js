741a987ba4e4ce92df046fde6aa47d0a1deb4e24
spencer-eagleton <86867967+spencer-eagleton@users.noreply.github.com>
2021-10-04 15:07:55 -0700

    declared variables from HTML, added math.random function

--- app.js
+++ app.js
@@ -1,6 +1,12 @@
 // import functions and grab DOM elements
+const userGuess = document.getElementById('user-guess');
+const button = document.getElementById('button');
+const resultMessage = document.getElementById('result-message');
 
 // initialize global state
+let randomNumber = [Math.floor(Math.random() * 20)];
+console.log(randomNumber)
+
 
 // set event listeners 
   // get user input
