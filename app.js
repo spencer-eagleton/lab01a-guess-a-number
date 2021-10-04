7a848c13a49ad03f26fda0c0eb2ed8e9bb64acf3
spencer-eagleton <86867967+spencer-eagleton@users.noreply.github.com>
2021-10-04 16:49:24 -0700

    added guesses left html, linked the span to JS to display guesses left

--- app.js
+++ app.js
@@ -2,16 +2,25 @@
 const userGuess = document.getElementById('user-guess');
 const button = document.getElementById('button');
 const resultMessage = document.getElementById('result-message');
+const guessesLeftDisplay = document.getElementById('guesses-left-display')
 
 // initialize global state
 let randomNumber = [Math.floor(Math.random() * 20)];
-console.log(randomNumber)
+  console.log(randomNumber)
+
+let guessesLeft = 4
+
+
+
+
 
 
 // set event listeners 
 button.addEventListener('click', ()=> {
-  console.log('click!')
-  console.log(userGuess.value)
+  guessesLeft --;
+  guessesLeftDisplay.textContent = guessesLeft
+  console.log(guessesLeft)
+  
   // get user input
   if(Number(userGuess.value) == randomNumber) {
     const resultText = "You guessed it!"
@@ -23,13 +32,16 @@ button.addEventListener('click', ()=> {
       const resultText = "Guess Higher!"
       resultMessage.textContent = resultText
 
+     
+
   } else if (Number(userGuess.value) > randomNumber) {
       
       const resultText = "Too high. Guess again!"
       resultMessage.textContent = resultText
   }
+   
   
-  
+ 
   
   // use user input to update state 
   // update DOM to reflect the new state
