2ff5af4e5fd097ada48766171908c8f21915a2d8
minoka kakizaki <minoka.kakizaki@gmail.com>
2021-10-08 15:49:49 -0700

    made it funky and refactoerd some functions, made it ended the game when you are out of guesses

--- app.js
+++ app.js
@@ -4,13 +4,22 @@ const guessButton = document.getElementById('guess-button');
 const resultMessage = document.getElementById('result-message');
 const guessesLeftDisplay = document.getElementById('guesses-left-display');
 const playAgain = document.getElementById('play-again');
+const hiddenImg = document.getElementById('dancing-guy');
+const winSpan = document.getElementById('wins');
+const lossSpan =document.getElementById('losses');
 
 // initialize global state
 let randomNumber = Math.floor(Math.random() * 20);
-console.log(randomNumber);
 
 let guessesLeft = 4;
 
+let wins = 0;
+let losses = 0;
+
+
+function rename(rDisplay, rText){
+    rDisplay.textContent = rText
+}
 
 
 
@@ -20,34 +29,41 @@ let guessesLeft = 4;
 guessButton.addEventListener('click', ()=> {
     guessesLeft --;
     guessesLeftDisplay.textContent = guessesLeft;
-   
+    console.log(randomNumber);
+    
   
   // get user input
     if (Number(userGuess.value) === randomNumber) {
-        console.log(randomNumber);
         const resultText = 'You guessed it!';
+        hiddenImg.classList.remove('hidden');
+        rename(resultMessage,  resultText);
+        wins ++;
+        winSpan.textContent= wins;
         
-        resultMessage.textContent = resultText;
-        console.log(resultText);
+
+
     } else if (guessesLeft === 0) {
         const resultText = 'Sorry, you\'re a loser, refresh page to try again';
-        resultMessage.textContent = resultText;
-
+        rename(resultMessage,  resultText);
+        losses ++;
+        lossSpan.textContent =losses;
+        
     } else if (guessesLeft < 0) {
         const resultText = 'Wow, you don\'t know when to give up. Hit refresh, loser!';
         resultMessage.textContent = resultText;
+        guessButton.classList.add('hidden');
+        
 
     } else if (Number(userGuess.value) < randomNumber) {
       
         const resultText = 'Guess Higher!';
-        resultMessage.textContent = resultText;
+        rename(resultMessage,  resultText);
 
      
-
     } else if (Number(userGuess.value) > randomNumber) {
       
         const resultText = 'Too high. Guess again!';
-        resultMessage.textContent = resultText;
+        rename(resultMessage,  resultText);
     }
    
     
@@ -59,12 +75,14 @@ guessButton.addEventListener('click', ()=> {
 
 playAgain.addEventListener('click', ()=>{
     
-    let randomNumber = Math.floor(Math.random() * 20);
-    console.log('random nummber:', randomNumber);
-
+    let randomNumber2 = Math.floor(Math.random() * 20);
     guessesLeft = 4;
-    
+    hiddenImg.classList.add('hidden');
     guessesLeftDisplay.textContent = 4;
+    resultMessage.textContent = '';
+    // console.log(randomNumber, randomNumber2)
+    randomNumber = randomNumber2;
+    return randomNumber
     
 
 });
